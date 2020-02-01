d23f5366aec9b0abdb1ad4fc911c55ade5c508fb
Ross Tyler <rossetyler@gmail.com>
2020-01-31 17:45:44 -0800

    2020

--- smartapps/rtyle/set-input-source.src/set-input-source.groovy
+++ smartapps/rtyle/set-input-source.src/set-input-source.groovy
@@ -2,7 +2,7 @@
 /**
  *	Set Input Source
  *
- *	Copyright 2019 Ross Tyler
+ *	Copyright 2020 Ross Tyler
  *
  *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *	in compliance with the License. You may obtain a copy of the License at:
